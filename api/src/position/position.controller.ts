4511c2bcf767ac606b105b6a274c9a83de06d17b
Dimentrius <lavrentevd579@gmail.com>
2024-11-10 11:17:44 +0300

    добавлен контроллер для crud над таблицей должностей

---
+++ api/src/position/position.controller.ts
@@ -0,0 +1,36 @@
+import { Controller, Get, Post, Patch, Delete, Req, Body } from '@nestjs/common';
+import { DbService } from 'src/db/db.service';
+import { PositionDto } from './position.dto';
+import { Request } from 'express';
+@Controller('position')
+export class PositionController {
+    constructor(private readonly dbService: DbService) {}
+    @Get()
+    async getPositions(){
+        const positions = this.dbService.query(`SELECT * FROM position`);
+        return positions;
+    }
+
+    @Get(':id')
+    async getPositionById(@Req() req: Request){
+        const position = this.dbService.query(`SELECT * FROM position WHERE id = ${req.params.id}`);
+        return position;
+    }
+
+    @Post()
+    async createPosition(@Body() positionDto: PositionDto){
+        const newPosition = this.dbService.query(`INSERT INTO organization (title) VALUES ('${positionDto.title}')`) 
+        return newPosition
+    }
+
+    @Patch(':id')
+    async updatePosition(@Req() req: Request, @Body() positionDto: PositionDto){
+        const updatedPosition = this.dbService.query(`UPDATE position SET title = '${positionDto.title}' WHERE id = ${req.params.id}`); 
+        return updatedPosition;
+    }
+    @Delete(':id')
+    async deletePosition(@Req() req: Request){
+        const deletingPosition = this.dbService.query(`DELETE FROM position WHERE id = ${req.params.id}`); 
+        return deletingPosition;
+    }
+}
