1039bd0a4e127025ac948d896a5e44a7de9985e5
Dimentrius <lavrentevd579@gmail.com>
2024-11-15 16:22:22 +0300

    добавлен eslintignore и изменён (настроен) конфиг eslint

--- api/.eslintrc.js
+++ api/.eslintrc.js
@@ -1,21 +1,29 @@
 module.exports = {
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: 'tsconfig.json',
-    tsconfigRootDir: __dirname,
-    sourceType: 'module',
-  },
-  plugins: ['@typescript-eslint/eslint-plugin'],
-  extends: [
-    'plugin:@typescript-eslint/recommended',
-    'plugin:prettier/recommended',
-  ],
   root: true,
+
+  parser: '@typescript-eslint/parser',
+
   env: {
     node: true,
     jest: true,
   },
-  ignorePatterns: ['.eslintrc.js'],
+
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: 'tsconfig.json',
+    tsconfigRootDir: __dirname,
+  },
+
+  plugins: ['@typescript-eslint/eslint-plugin'],
+
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    // 'plugin:prettier/recommended',
+  ],
+
+  ignorePatterns: ['dist', '.eslintrc.js'],
+
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
