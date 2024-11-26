7f92f9b4f4a16853c5177303fdefad0146d04ba6
Dimentrius <lavrentevd579@gmail.com>
2024-11-26 17:36:26 +0300

    добавлены зависимости в package.json

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -7,10 +7,19 @@ import { DbModule } from './db/db.module';
 import { OrganizationController } from './organization/organization.controller';
 import { DepartmentController } from './department/department.controller';
 import { PositionController } from './position/position.controller';
+import { EmployeeController } from './employees/employee.controller';
+import { FilesController } from './files_crud/file.controller';
+import { OperationController } from './hr_operations/operation.controller';
+import { ArrangedEmployeeController } from './hr_operations/arrangedEmployees.controller';
 
 import { OrganizationService } from './organization/organization.service';
 import { DepartmentService } from './department/department.service';
 import { PositionService } from './position/position.service';
+import { EmployeeService } from './employees/employee.service';
+import { FileService } from './files_crud/file.service';
+import { OperationService } from './hr_operations/operation.service';
+
+
 @Module({
     imports: [
         DbModule.register({
@@ -26,12 +35,19 @@ import { PositionService } from './position/position.service';
         OrganizationController,
         DepartmentController,
         PositionController,
+        EmployeeController,
+        FilesController,
+        OperationController,
+        ArrangedEmployeeController
     ],
     providers: [
         AppService,
         OrganizationService, 
         DepartmentService, 
-        PositionService
+        PositionService,
+        EmployeeService,
+        FileService,
+        OperationService
     ],
 })
 export class AppModule {}
